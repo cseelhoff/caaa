ba368216651f09d0bcb1307f11b19028f320cb89
Caleb Seelhoff <cseelhoff@gmail.com>
2024-09-12 18:20:02 +0000

    git test

---
+++ CMakeFiles/caaa.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/caaa.dir/src/canal.c.o"
+  "CMakeFiles/caaa.dir/src/canal.c.o.d"
+  "CMakeFiles/caaa.dir/src/engine.c.o"
+  "CMakeFiles/caaa.dir/src/engine.c.o.d"
+  "CMakeFiles/caaa.dir/src/game_state.c.o"
+  "CMakeFiles/caaa.dir/src/game_state.c.o.d"
+  "CMakeFiles/caaa.dir/src/land.c.o"
+  "CMakeFiles/caaa.dir/src/land.c.o.d"
+  "CMakeFiles/caaa.dir/src/main.c.o"
+  "CMakeFiles/caaa.dir/src/main.c.o.d"
+  "CMakeFiles/caaa.dir/src/mcts.c.o"
+  "CMakeFiles/caaa.dir/src/mcts.c.o.d"
+  "CMakeFiles/caaa.dir/src/player.c.o"
+  "CMakeFiles/caaa.dir/src/player.c.o.d"
+  "CMakeFiles/caaa.dir/src/sea.c.o"
+  "CMakeFiles/caaa.dir/src/sea.c.o.d"
+  "CMakeFiles/caaa.dir/src/serialize_data.c.o"
+  "CMakeFiles/caaa.dir/src/serialize_data.c.o.d"
+  "CMakeFiles/caaa.dir/src/team.c.o"
+  "CMakeFiles/caaa.dir/src/team.c.o.d"
+  "CMakeFiles/caaa.dir/src/units.c.o"
+  "CMakeFiles/caaa.dir/src/units.c.o.d"
+  "caaa"
+  "caaa.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/caaa.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/caaa.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for caaa.
+# This may be replaced when dependencies are built.
